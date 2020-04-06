5f88fca4c4519967e2eef524a70c7552bef6ff9e
naou <monaou@gmail.com>
2020-04-06 21:29:57 +0900

    Update dependencies and add test on DockerHub

---
+++ test/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+cd `dirname $0`
+cat <<-EOF | sphinx-quickstart
+y
+prohect
+author
+release
+ja
+EOF
+echo "" >> ./source/index.rst
+echo ".. uml::" >> ./source/index.rst
+echo "    " >> ./source/index.rst
+echo "    Hogeほげ --> Piyoぴよ" >> ./source/index.rst
+
+echo "" >> ./source/conf.py
+echo "extensions += ['sphinxcontrib.plantuml']" >> ./source/conf.py
+echo "plantuml = 'java -jar /opt/plantuml/plantuml.jar'" >> ./source/conf.py
+
+make html
+
+cat ./build/html/index.html | grep "<img" | grep ".png" || exit 1
